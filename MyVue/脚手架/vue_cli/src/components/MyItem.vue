6ce9f839deb15eebd1fdb708a35630e3b7b6c205
liqiang <1715991019@qq.com>
2022-04-14 00:10:17 +0800

    dormitory night

---
+++ MyVue/脚手架/vue_cli/src/components/MyItem.vue
@@ -0,0 +1,58 @@
+<template>
+    <li>
+        <label>
+            <!-- checked表示默认选中复选框，checked这里有没有，取决于todo.done
+                done=true，就有checked
+                :checked 是伪类选择器，当它的值为true时，就有效，为false无效 -->
+            <input type="checkbox" :checked='todo.done'/>
+            <span>{{todo.title}}</span>
+        </label>
+        <button class="btn btn-danger" style="display:none">删除</button>
+    </li>
+</template>
+
+<script>
+export default {
+    name:'MyItem',
+    // 接收todo对象
+    props:['todo']
+}
+</script>
+
+<style scoped>
+    /*item*/
+    li {
+    list-style: none;
+    height: 36px;
+    line-height: 36px;
+    padding: 0 5px;
+    border-bottom: 1px solid #ddd;
+    }
+
+    li label {
+    float: left;
+    cursor: pointer;
+    }
+
+    li label li input {
+    vertical-align: middle;
+    margin-right: 6px;
+    position: relative;
+    top: -1px;
+    }
+
+    li button {
+    float: right;
+    display: none;
+    margin-top: 3px;
+    }
+
+    li:before {
+    content: initial;
+    }
+
+    li:last-child {
+    border-bottom: none;
+    }
+
+</style>
---
+++ MyVue/脚手架/vue_cli/src/components/MyList.vue
@@ -0,0 +1,47 @@
+<template>
+    <ul class="todo-main">
+        <!-- v-for遍历对象， :todo声明一个属性绑定到v-for中的todo对象-->
+        <MyItem v-for="todo in todoList" :key="todo.id" :todo='todo'/>
+    </ul>
+</template>
+
+<script>
+import MyItem from './MyItem.vue'
+export default {
+    name:'MyList',
+    data() {
+        return {
+            // 在数组里面存对象
+            todoList:[
+                // id使用string，不使用number类型，因为number有尽头，string没有
+                // 真正开发中用的都是hash值
+                {id:'001', title:'抽烟',done:true },
+                {id:'002', title:'喝酒',done:false },
+                {id:'003', title:'开车',done:true },
+            ]
+        }
+    },
+    components:{MyItem}
+}
+</script>
+
+<style scoped>
+    /*main*/
+    .todo-main {
+    margin-left: 0px;
+    border: 1px solid #ddd;
+    border-radius: 2px;
+    padding: 0px;
+    /* 去掉ul的列表项符号 */
+    list-style-type: none;
+    }
+
+    .todo-empty {
+    height: 40px;
+    line-height: 40px;
+    border: 1px solid #ddd;
+    border-radius: 2px;
+    padding-left: 5px;
+    margin-top: 10px;
+    }
+</style>
